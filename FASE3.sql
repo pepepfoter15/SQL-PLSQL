330f857e9d7953b020c85f97dfec2de13ae5a2e2
José Carlos Rodríguez Cañas <119460713+pepepfoter15@users.noreply.github.com>
2023-11-07 19:45:41 +0100

    Fase3

--- FASE3.sql
+++ FASE3.sql
@@ -106,5 +106,29 @@ GROUP BY
 
 
 --9. Muestra los propietarios que nunca han tenido uno de sus caballos en el podio.
+SELECT
+    p.dni,
+    p.nombre AS NombrePropietario
+FROM
+    propietarios p
+WHERE
+    p.dni NOT IN (
+        SELECT DISTINCT c.dniPropietario
+        FROM
+            caballos c
+        JOIN participaciones pa ON c.codigoCaballo = pa.codigoCaballo
+        WHERE
+            pa.posicionFinal IN (1, 2, 3)
+    );
+
+--Prueba de que  el propietario tenga un caballo no metido en el podio es insertando otro nuevo ya que todos han estado previamente en el podio.
+insert into propietarios
+values ('X77056225B', 'Pepe', 'Gutierrez', ' ',150);
+
+insert into caballos
+values (11,'X77056225B', 'Dagoberto', '9-10-2010','Purasangre español' );
+
+insert into participaciones
+values(1, 11, 'Y6857984L', 24, 7);
 
 --10.Crea una vista en la que aparezca la carrera que ha hecho ganar más dinero a cada uno de los clientes, junto con el importe de los beneficios obtenidos.

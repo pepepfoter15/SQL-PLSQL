268ff39288ee5e35ebc0aee93ae54ef687892321
José Carlos Rodríguez Cañas <119460713+pepepfoter15@users.noreply.github.com>
2023-11-07 18:21:02 +0100

    Fase3

---
+++ FASE3.sql
@@ -0,0 +1,21 @@
+--1. El jockey de menor estatura va a participar en la última carrera de la temporada con el caballo que más veces utilizó las instalaciones del hipódromo en el año 2014. Su dorsal será el 23 y todavía no sabemos en que posición acabará. Inserta el registro correspondiente.
+
+
+
+--2. El caballo más joven del propietario que ha ganado más carreras entre todos sus caballos ha engordado siete kilos. Registra el cambio en la base de datos.
+
+--3. Muestra el importe total de las apuestas de cada uno de los caballos que participaron en la primera carrera de la temporada 2014.
+
+--4. Muestra los datos del cliente que ha ganado más dinero con las apuestas en los tres últimos meses.
+
+--5. Muestra los propietarios de caballos que hayan ocupado todas y cada una de las tres posiciones del podio en las diferentes carreras en las que hayan participado.
+
+--6. Muestra el número de abandonos de cada uno de los caballos, incluyendo aquéllos que no hayan sufrido ningún abandono.
+
+--7. Muestra para cada carrera el caballo más joven que ha participado en la misma.
+
+--8. Muestra el último caballo que ganó una carrera de cada uno de los propietarios.
+
+--9. Muestra los propietarios que nunca han tenido uno de sus caballos en el podio.
+
+--10.Crea una vista en la que aparezca la carrera que ha hecho ganar más dinero a cada uno de los clientes, junto con el importe de los beneficios obtenidos.

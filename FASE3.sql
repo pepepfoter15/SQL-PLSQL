c0cb291aeab43f29a7bd03d44fbdde9da85d1362
José Carlos Rodríguez Cañas <119460713+pepepfoter15@users.noreply.github.com>
2023-11-07 19:06:11 +0100

    Fase3

--- FASE3.sql
+++ FASE3.sql
@@ -38,9 +38,23 @@ WHERE codigoCaballo = '1';
 
 --4. Muestra los datos del cliente que ha ganado más dinero con las apuestas en los tres últimos meses.
 
+
 --5. Muestra los propietarios de caballos que hayan ocupado todas y cada una de las tres posiciones del podio en las diferentes carreras en las que hayan participado.
 
 --6. Muestra el número de abandonos de cada uno de los caballos, incluyendo aquéllos que no hayan sufrido ningún abandono.
+--Muestra los caballos abandonados haciendo referencia a que si no tienen codigo de carrera, están sin dueño.
+SELECT
+    c.codigoCaballo,
+    c.nombre AS NombreCaballo,
+    COUNT(p.codigoCarrera) AS NumeroAbandonos
+FROM
+    caballos c
+LEFT JOIN participaciones p ON c.codigoCaballo = p.codigoCaballo AND p.posicionFinal = -1
+GROUP BY
+    c.codigoCaballo, c.nombre
+ORDER BY
+    c.codigoCaballo;
+
 
 --7. Muestra para cada carrera el caballo más joven que ha participado en la misma.
 

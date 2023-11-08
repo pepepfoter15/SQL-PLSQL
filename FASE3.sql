334081153fe2b71f0fdab8c4e8c97e0208d581cb
Gonzalo Peña Calero <98739553+Gpc23@users.noreply.github.com>
2023-11-08 08:28:34 +0100

    Ejercicio 10

--- FASE3.sql
+++ FASE3.sql
@@ -132,3 +132,20 @@ insert into participaciones
 values(1, 11, 'Y6857984L', 24, 7);
 
 --10.Crea una vista en la que aparezca la carrera que ha hecho ganar más dinero a cada uno de los clientes, junto con el importe de los beneficios obtenidos.
+CREATE VIEW vista_ganancias_clientes AS
+SELECT c.dni AS cliente_dni, c.nombre AS cliente_nombre, c.apellido1 AS cliente_apellido1, c.apellido2 AS cliente_apellido2,
+       a.dniCliente, a.codigoCarrera,
+       cp.fechaHora AS fecha_carrera,
+       SUM(a.importeApostado * a.tantoAUno) AS ganancias
+FROM clientes c
+JOIN apuestas a ON c.dni = a.dniCliente
+JOIN carrerasProfesionales cp ON a.codigoCarrera = cp.codigoCarrera
+GROUP BY c.dni, c.nombre, c.apellido1, c.apellido2, a.dniCliente, a.codigoCarrera, cp.fechaHora
+HAVING SUM(a.importeApostado * a.tantoAUno) = (
+  SELECT MAX(SUM(a2.importeApostado * a2.tantoAUno))
+  FROM apuestas a2
+  JOIN carrerasProfesionales cp2 ON a2.codigoCarrera = cp2.codigoCarrera
+  WHERE a2.dniCliente = a.dniCliente
+  GROUP BY a2.dniCliente, a2.codigoCarrera
+)
+ORDER BY c.dni, ganancias DESC;

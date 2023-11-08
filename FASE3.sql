00fb1b7a2ea28244b996ec66f4dd0d8bdaa8d68f
Josemanuel4f <52412226+Josemanuel4f@users.noreply.github.com>
2023-11-08 14:16:39 +0100

    Añadido ejercicio 3 fase3

--- FASE3.sql
+++ FASE3.sql
@@ -55,6 +55,24 @@ SET peso = peso + 7
 WHERE codigoCaballo = '1';
 
 --3. Muestra el importe total de las apuestas de cada uno de los caballos que participaron en la primera carrera de la temporada 2014.
+--(Ya que no hay datos en 2014 he usado los datos que existian y he usado el año 2017 )
+
+--Primera carrera
+create or replace view primcarrera
+as select * from carrerasprofesionales where extract(year from fechaHora) = 2017
+order by fechaHora ASC
+fetch first 1 row only;
+
+--ver los caballos q participaron en esa carrera:
+create or replace view participantes
+as select codigocaballo from participaciones 
+where codigoCarrera = (select codigocarrera from primcarrera);
+
+--Importe apostado por caballo
+select codigocaballo, sum(importeapostado) as Total_Apostado from apuestas 
+where codigocaballo in (select codigocaballo from participantes) 
+group by codigocaballo;
+
 
 --4. Muestra los datos del cliente que ha ganado más dinero con las apuestas en los tres últimos meses.
 

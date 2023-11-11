eb55aa53f49cece8e1db697296060888c8fb640f
Josemanuel4f <52412226+Josemanuel4f@users.noreply.github.com>
2023-11-11 15:45:51 +0100

    Añadido el ejercicio que quedaba por terminar, el ejercicio 4.

--- FASE3.sql
+++ FASE3.sql
@@ -80,16 +80,29 @@ group by codigocaballo;
 
 --4. Muestra los datos del cliente que ha ganado más dinero con las apuestas en los tres últimos meses.
 --HECHO POR JOSEMA.
---caballos que han ganado una carrera:
+--He insertado datos nuevos actuales, como nuevas apuestas y nuevas carreras realizadas en los 3 ultimos meses.
+
+--Carreras de los ultimos 3 meses.
+create view ultimascarreras
+as select * from carrerasprofesionales 
+where fechahora >= sysdate - interval '3' month;
+
+--caballos que han ganado una carrera de los ultimos 3 meses
 create or replace view ganadores
-as select * from participaciones where posicionfinal = 1;
+as select p.codigocaballo, p.codigocarrera 
+from participaciones p, ultimascarreras c 
+where posicionfinal = 1 and c.codigocarrera=p.codigocarrera;
 
---apuestas ganadas:
-select * from apuestas where codigocaballo in (select codigocaballo from ganadores) and
-codigocarrera in (select codigocarrera from ganadores);
+--apuestas ganadas, que mas dinero ha ganado en los ultimos 3 meses.:
+create or replace view apuestasganadas
+as select dnicliente, importeapostado * tantoAUno as ganancias from apuestas 
+where (codigocarrera, codigocaballo) in (select codigocarrera, codigocaballo from ganadores)
+order by ganancias DESC
+fetch first 1 row only;
 
-insert into apuestas
-values ('28441115n',1 ,2 ,300 ,3.10);
+--Mostrar datos cliente:
+select * from clientes 
+where dni = (select dnicliente from apuestasganadas);
 
 
 --5. Muestra los propietarios de caballos que hayan ocupado todas y cada una de las tres posiciones del podio en las diferentes carreras en las que hayan participado.

abd872991f4040a595f022b2081680835206c590
Mario-Zayas <123730044+Mario-Zayas@users.noreply.github.com>
2023-11-17 14:28:37 +0100

    Añadir Ejercicio 1 (Gonzalo y Mario)

--- FASE4.sql
+++ FASE4.sql
@@ -1,74 +1,92 @@
 1. Realiza una función que reciba como parámetros un código de carrera y un código de caballo y devuelva el importe total que tendrá que pagar el hipódromo a los apostantes suponiendo que dicha carrera sea ganada por el caballo recibido como parámetro. Se deben contemplar las siguientes excepciones: Carrera inexistente, Caballo inexistente, Caballo no participante en esa carrera. 
 
-CREATE OR REPLACE FUNCTION importetotal (
-    p_codcarrera apuestas.codigocarrera%type,
-    p_codcaballo apuestas.codigocaballo%type
-) RETURN NUMBER IS
-    v_mult NUMBER;
-BEGIN
-    comprobarvacias(p_codcarrera, p_codcaballo);
+create or replace function importe (
+    p_codigocarrera apuestas.codigocarrera%type,
+    p_codigocaballo apuestas.codigocaballo%type) 
+	return number is
+    v_total number;
+begin
 
-    SELECT importeapostado * tantoauno INTO v_mult
-    FROM apuestas
-    WHERE codigocarrera = p_codcarrera AND codigocaballo = p_codcaballo;
+    select importeApostado * tantoAUno into v_total
+    from apuestas
+    where codigocarrera = p_codigocarrera and codigocaballo = p_codigocaballo;
 
-    dbms_output.put_line('Hay que pagar al ganador un total de ' || v_mult || ' euros');
-    
-    RETURN v_mult;
-END;
+    RETURN v_total;
+end;
 /
 
-CREATE OR REPLACE PROCEDURE comprobarvacias (
-    p_codcarrera apuestas.codigocarrera%type,
-    p_codcaballo apuestas.codigocaballo%type
-) IS
-    v_carrera        NUMBER;
-    v_caballo        NUMBER;
-    v_caballocarrera NUMBER;
-BEGIN
-    SELECT COUNT (*) INTO v_carrera
-    FROM apuestas
-    WHERE codigocarrera = p_codcarrera;
+CREATE OR REPLACE PROCEDURE comprobarcarrera (
+    p_codigocarrera apuestas.codigocarrera%type,
+    p_codigocaballo apuestas.codigocaballo%type) 
+	is
+    v_carrera number;
+begin
+    select count (*) INTO v_carrera
+    from apuestas
+    where codigocarrera = p_codigocarrera;
 
-    IF v_carrera = 0 THEN
+    if v_carrera = 0 then
         raise_application_error(-20112, 'No existe la carrera');
-    END IF;
-
-    SELECT COUNT (*) INTO v_caballo
-    FROM apuestas
-    WHERE codigocaballo = p_codcaballo;
-
-    IF v_caballo = 0 THEN
-        raise_application_error(-20110, 'No se ha encontrado el caballo');
-    END IF;
-
-    SELECT COUNT (*) INTO v_caballocarrera
-    FROM participaciones
-    WHERE codigocarrera = p_codcarrera AND codigocaballo = p_codcaballo;
-    
-    IF v_caballocarrera = 0 THEN
-        raise_application_error(-20103, 'No existen caballos en esas carreras');
-    END IF;
-dbms_output.put_line('Comprobado que están vacias');
-END;
+    end if;
+end;
 /
 
-Funcionamiento de la función: 
+CREATE OR REPLACE PROCEDURE comprobarcaballo (
+    p_codigocarrera apuestas.codigocarrera%type,
+    p_codigocaballo apuestas.codigocaballo%type) 
+	is
+    v_caballo number;
+begin
+    select count (*) INTO v_caballo
+    from apuestas
+    where codigocaballo = p_codigocaballo;
 
-DECLARE
-    v_resultado NUMBER;
+    if v_caballo = 0 then
+        raise_application_error(-20110, 'No se ha encontrado el caballo');
+    end if;
+end;
+/
+
+CREATE OR REPLACE PROCEDURE comprobarexiste (
+    p_codigocarrera apuestas.codigocarrera%type,
+    p_codigocaballo apuestas.codigocaballo%type) 
+	is
+    v_caballocarrera number;
+begin
+    select count (*) INTO v_caballocarrera
+    from participaciones
+    where codigocarrera = p_codigocarrera AND codigocaballo = p_codigocaballo;
+    
+    if v_caballocarrera = 0 then
+        raise_application_error(-20103, 'No existen caballos en esas carreras');
+    end if;
+end;
+/
+
+CREATE OR REPLACE PROCEDURE Mostrar_Total (
+    p_codigocarrera apuestas.codigocarrera%type,
+    p_codigocaballo apuestas.codigocaballo%type)
+IS
+    importe_total number;
 BEGIN
-    v_resultado := importetotal('1', '2');
-    dbms_output.put_line('El importe total es: ' || v_resultado || ' euros');
+    comprobarcarrera(p_codigocarrera, p_codigocaballo);
+    comprobarcaballo(p_codigocarrera, p_codigocaballo);
+    comprobarexiste(p_codigocarrera, p_codigocaballo);
+    importe_total := importe(p_codigocarrera, p_codigocaballo);
+
+    dbms_output.put_line('Hay que pagar al ganador un total de ' || importe_total || ' euros');
 END;
 /
 
 Funcionamiento del procedimiento:
 
-EXEC comprobarvacias('0', '2');
-EXEC comprobarvacias('2', '0');
-EXEC comprobarvacias('2', '3');
-EXEC comprobarvacias('2', '7');
+exec comprobarcarrera('1','2');
+exec comprobarcarrera('0','2');
+exec comprobarcaballo('1','2');
+exec comprobarcaballo('2','0');
+exec comprobarexiste('2','2');
+exec comprobarexiste('12','7');
+exec Mostrar_Total('12','7');
 
 3. Realiza un trigger que controle que solo participan en una carrera caballos en el rango de edades permitido. Si es necesario, cambia el modelo de datos. 
 
